61fe35bcee149b7b8719134ff65b2541e91cf52a
User Name <wafaakhawaldah88@gmail.com>
2021-06-07 15:06:04 +0300

    static

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var express = require('express')
+var app = express()
+const bodyparser= require("body-parser")
+const  bodyParserMiddleWare= bodyparser.urlencoded({extended: true})
+const path = require("path")
+app.use(express.static(path.join(__dirname + '/')));
+
+app.use(express.static('css'))//external css
+/*
+app.get('/node.css', function(req, res, next) {
+    res.sendFile(__dirname + "/" + "node.css");
+  });// internal css
+*/
+app.get('/', (req, res, next)=>{
+    res.sendFile(path.join(__dirname, "page1.html"))
+  })
+
+app.get('/', (req, res, next)=>{
+    res.sendFile(path.join(__dirname, "node.html"))
+  })
+
+  app.post("/info", bodyParserMiddleWare, (req, res, next)=>{
+      console.log(req.body)
+      res.sendFile(path.join(__dirname, "message.text"))   
+  })
+app.listen(3000,()=>{ 
+    console.log("Server listening on port: 3000");
+});
